2cdbedc766ca19e58425177c7a8b607e8e956c5d
Justinldub <justinldube31@gmail.com>
2024-02-14 13:44:24 +0200

    c1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,55 +1,43 @@
+/*
+ * File: 0-read_textfile.c
+ * Auth: Dr Marcus.
+ */
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * Author: Justin Dube
- * read_textfile - Function that reads a text file and print to the STD_OUT
- * Then closes the files and frees the allocated memmory
- * @filename: The name of the file to be loaded from the file input stream
- * @letters: the size of the letters to be able to read
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/*Create variable*/
-	FILE *file;
+	ssize_t o, r, w;
 	char *buffer;
-	ssize_t bytes_read;
 
-	/*Opening file*/
-	file = fopen(filename, "r");
-	if(file == NULL)
-	{
-		perror("Could not open file specified");
-		return -1;
-	}
+	if (filename == NULL)
+		return (0);
 
-	/*Allocate memory to buffer*/
-	buffer = (char *)malloc(sizeof(char) * (letters + 1));
-	if(buffer == NULL)
-	{
-		perror("Error allocating memory");
-		fclose(file);
-		return -1;
-	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-	/*Read from file into the buffer*/
-	bytes_read = fread(buffer, sizeof(char),letters, file);
-	if(bytes_read == -1)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		perror("counld not read from the file");
-		fclose(file);
 		free(buffer);
-		return -1;
+		return (0);
 	}
 
-	/*Null terminate the buffer*/
-	buffer[bytes_read] = '\0';
-	printf("%s",buffer);
-
-	/*Free resources*/
-	fclose(file);
 	free(buffer);
+	close(o);
 
-	return bytes_read;
-
+	return (w);
 }
-
-
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,44 +1,32 @@
 #include "main.h"
 /**
- * @Author: Justin Dube
- * @Date: 2024-02-13
- * @disc: This algorith create a new file in the file system and write text into it.
- * =================================================================================
- * create_file - function to create a new text file or truncate a file.
- * Sets permissions to rw-------
- * Writes text content into the file
- * =================================================================================
- * @filename: the name of the file to be created.
- * @text_content: the text to be written into the file.
- * Return: -1 on failure other 1 on success.
+ * create_file -creates an array of chars, and initializes
+ *
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	if(filename == NULL)
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
 	{
-		return -1;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	int fd;
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if(fd == -1)
-	{
-		perror("Failed to open or create a file");
-		return -1;
-	}
+	if (o == -1 || w == -1)
+		return (-1);
 
-	if(text_content != NULL)
-	{
-		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-		if(bytes_written == -1)
-		{
-			perror("Failed to write to file");
-			close(fd);
-			return -1;
-		}
-	}
+	close(o);
 
-	close(fd);
-	return 1;
+	return (1);
 }
